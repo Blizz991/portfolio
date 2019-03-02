b8278cb66c00a40107f1bf04a3cee26d8eb9b051
Martin Høite <martinhblizz@gmail.com>
2019-03-03 00:50:07 +0100

    Temp logo added. Initial setup of portfolio page. Ready to start making content.

--- js/Main.js
+++ js/Main.js
@@ -3,7 +3,15 @@
   
       $('.sidenav').sidenav();
       $('.parallax').parallax();
-  
+      $('.pushpin').pushpin();
+      $('.pushpin-nav').each(function() {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+          top: $target.offset().top,
+          bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
+      });
     }); // end of document ready
   })(jQuery); // end of jQuery name space
   
